f2d7178a2588c9b12bed4b64f83a08b147b6ae0c
fahad-essa1 <92952722+fahad-essa1@users.noreply.github.com>
2024-05-13 07:45:41 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,237 +1,103 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
 import 'dart:math';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MemoryGame());
+  runApp(RockPaperScissorsApp());
 }
 
-class MemoryGame extends StatelessWidget {
+class RockPaperScissorsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Memory Game',
+      title: 'Rock Paper Scissors',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MemoryGameScreen(),
+      home: GameScreen(),
     );
   }
 }
 
-class MemoryGameScreen extends StatefulWidget {
+class GameScreen extends StatefulWidget {
   @override
-  _MemoryGameScreenState createState() => _MemoryGameScreenState();
+  _GameScreenState createState() => _GameScreenState();
 }
 
-class _MemoryGameScreenState extends State<MemoryGameScreen> {
-  List<String> symbols = ['🌼', '🍎', '🍕', '🚀', '🎈', '🐱', '🐳', '🎸'];
-  List<String> pairs = [];
-  List<bool> revealed = [];
-  int attempts = 0;
-  int pairsFound = 0;
-  int selectedCardIndex = -1;
-  int level = 0; // 0: Easy, 1: Medium, 2: Hard
-  Stopwatch stopwatch = Stopwatch();
-  Timer? timer;
+class _GameScreenState extends State<GameScreen> {
+  String playerChoice = '';
+  String computerChoice = '';
+  String result = '';
 
-  @override
-  void initState() {
-    super.initState();
-    _restartGame();
-    startTimer();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    stopwatch.stop();
-    timer?.cancel();
-  }
-
-  void _restartGame() {
+  void _getPlayerChoice(String choice) {
     setState(() {
-      attempts = 0;
-      pairsFound = 0;
-      selectedCardIndex = -1;
-      stopwatch.reset();
-      stopwatch.start();
-      int cardsCount = _getNumberOfCards();
-      pairs = List<String>.generate(cardsCount ~/ 2, (index) => symbols[index % symbols.length])
-        ..addAll(List<String>.generate(cardsCount ~/ 2, (index) => symbols[index % symbols.length]));
-      pairs.shuffle();
-      revealed = List.filled(cardsCount, false);
+      playerChoice = choice;
+      _getComputerChoice();
+      _calculateResult();
     });
   }
 
-  int _getNumberOfCards() {
-    switch (level) {
-      case 0:
-        return 4 * 2; // Easy
-      case 1:
-        return 6 * 2; // Medium
-      case 2:
-        return 8 * 2; // Hard
-      default:
-        return 4 * 2;
+  void _getComputerChoice() {
+    List<String> choices = ['rock', 'paper', 'scissors'];
+    final random = Random();
+    computerChoice = choices[random.nextInt(choices.length)];
+  }
+
+  void _calculateResult() {
+    if (playerChoice == computerChoice) {
+      result = 'It\'s a tie!';
+    } else if ((playerChoice == 'rock' && computerChoice == 'scissors') ||
+        (playerChoice == 'paper' && computerChoice == 'rock') ||
+        (playerChoice == 'scissors' && computerChoice == 'paper')) {
+      result = 'You win!';
+    } else {
+      result = 'You lose!';
     }
-  }
-
-  void _onCardTap(int index) {
-    setState(() {
-      if (selectedCardIndex == -1) {
-        selectedCardIndex = index;
-        revealed[index] = true;
-      } else {
-        if (pairs[selectedCardIndex] == pairs[index] && selectedCardIndex != index) {
-          revealed[index] = true;
-          pairsFound++;
-          if (pairsFound == _getNumberOfCards() ~/ 2) {
-            _showWinDialog();
-          }
-        } else {
-          revealed[selectedCardIndex] = false;
-          revealed[index] = false;
-        }
-        selectedCardIndex = -1;
-        attempts++;
-      }
-    });
-  }
-
-  void startTimer() {
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      if (!mounted) return;
-      setState(() {});
-    });
-  }
-
-  void _showWinDialog() {
-    stopwatch.stop();
-    timer?.cancel();
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Congratulations!'),
-          content: Text('You found all pairs in $attempts attempts in ${stopwatch.elapsed.inSeconds} seconds.'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        title: Text('Rock Paper Scissors'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Memory Game - Level: ${['Easy', 'Medium', 'Hard'][level]}',
-              overflow: TextOverflow.ellipsis,
+              'Choose your move:',
+              style: TextStyle(fontSize: 20),
             ),
+            SizedBox(height: 20),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text('Attempts: $attempts'),
-                SizedBox(width: 16),
-                Text('Time: ${stopwatch.elapsed.inSeconds} seconds'),
+                ElevatedButton(
+                  onPressed: () => _getPlayerChoice('rock'),
+                  child: Text('Rock'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _getPlayerChoice('paper'),
+                  child: Text('Paper'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _getPlayerChoice('scissors'),
+                  child: Text('Scissors'),
+                ),
               ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Computer chose: $computerChoice',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Result: $result',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: _restartGame,
-          ),
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bac5.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              double cardSize = min(constraints.maxWidth, constraints.maxHeight) * 0.15;
-              return GridView.builder(
-                padding: EdgeInsets.all(cardSize * 0.1),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: cardSize * 0.1,
-                  mainAxisSpacing: cardSize * 0.1,
-                ),
-                itemCount: _getNumberOfCards(),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      _onCardTap(index);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      width: cardSize,
-                      height: cardSize,
-                      decoration: BoxDecoration(
-                        color: revealed[index] ? Colors.white : Colors.blue,
-                        borderRadius: BorderRadius.circular(cardSize * 0.1),
-                        boxShadow: revealed[index]
-                            ? [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            blurRadius: cardSize * 0.2,
-                            offset: Offset(0, cardSize * 0.4),
-                          ),
-                        ]
-                            : [],
-                      ),
-                      child: Center(
-                        child: Text(
-                          revealed[index] ? pairs[index] : '',
-                          style: TextStyle(fontSize: cardSize * 0.6, color: Colors.black),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: level,
-        onTap: (int index) {
-          setState(() {
-            level = index;
-            _restartGame();
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.child_care),
-            label: 'Easy',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.accessibility_new),
-            label: 'Medium',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.accessible_forward),
-            label: 'Hard',
-          ),
-        ],
       ),
     );
   }
